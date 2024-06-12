084a0164da1fe5bbc54451a9876e65fea87aec72
keanu <keanucarlo@hotmail.com>
2024-06-12 16:30:17 +0200

    added random monster generator

--- TurnbasedRPG/main.cpp
+++ TurnbasedRPG/main.cpp
@@ -3,15 +3,26 @@
 #include "combatsystem.h"
 #include "entity.h"
 #include "fightercomponent.h"
+#include "uiDriver.h"
 
 int main()
 {
     Entity combatSystemEntity;
     CombatSystem* combatSystem = combatSystemEntity.AddComponent<CombatSystem>();
 
-    while (!combatSystem->IsGameOver())
+    while (false&&!combatSystem->IsGameOver())
     {
         combatSystem->Update();
+    }
+
+    while (true)
+    {
+        system("cls");
+        uiDriver uiDriver;
+        uiDriver.displayMonster();
+
+        
+        system("pause");
     }
 
     return 0;
--- TurnbasedRPG/uiDriver.cpp
+++ TurnbasedRPG/uiDriver.cpp
@@ -1,1 +1,28 @@
 #include "uiDriver.h"
+#include <cstdlib> // For rand() and srand()
+#include <ctime>   // For time()
+
+void uiDriver::displayMonster() 
+{
+    int firstMonster = rand() % 10;
+    int secondMonster = rand() % 10;
+    int thirdMonster = rand() % 10;
+    int fourthMonster = rand() % 10;
+
+    std::string lines[7];
+
+    for (int i = 0; i < 7; i++)
+    {
+        lines[i] += monsterGrafix[firstMonster][i];
+        lines[i] += monsterGrafix[secondMonster][i];
+        lines[i] += monsterGrafix[thirdMonster][i];
+        lines[i] += monsterGrafix[fourthMonster][i];
+
+        lines[i] += "|";
+    }
+
+    for (int i = 0; i < 7; i++)
+    {
+        std::cout << lines[i] << std::endl;
+    }
+}
